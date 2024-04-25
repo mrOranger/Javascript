bfed2b28617d0507114954925416343c3668e3af
Edoardo Oranger <edo.oranger@gmail.com>
2024-04-25 09:07:47 +0200

    Completed chapter for objects

--- 6. Objects/object.controller.js
+++ 6. Objects/object.controller.js
@@ -6,26 +6,28 @@ angular.module('ObjectModule').controller('ObjectController', function ObjectCon
       this.checkFormValidity = function checkFormValidity() {
             if (this.employee && this.property) {
                   if (this.employee.firstName && this.employee.lastName && this.property.key && this.property.value) {
-                        if (
+                        return (
                               this.employee.firstName.trim().length > 0 &&
                               this.employee.lastName.trim().length > 0 &&
                               this.property.key.trim().length > 0 &&
                               this.property.value.trim().length > 0
-                        ) {
-                              return true;
-                        }
+                        );
                   }
             }
             return false;
       };
 
-      this.onAddNewEmployee = function onAddNewEmployee(employee, property) {
-            if (employee && property) {
-                  this.employees.splice(0, 0, {
-                        firstName: employee.firstName,
-                        lastName: employee.lastName,
-                        [property.key]: property.value,
+      this.onAddNewEmployee = function onAddNewEmployee() {
+            if (this.employee && this.property) {
+                  this.employees.splice(this.employees.length, 0, {
+                        'first name': this.employee.firstName,
+                        'last name': this.employee.lastName,
+                        [this.property.key]: this.property.value,
                   });
             }
+      };
+
+      this.onRemoveEmployee = function onRemoveEmployee(index) {
+            this.employees.splice(index, 1);
       };
 });
