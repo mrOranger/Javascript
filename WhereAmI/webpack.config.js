3c73df079ebc010b63d8cb04aa04bc483f612ea4
Edoardo Oranger <edo.oranger@gmail.com>
2024-06-01 16:05:51 +0200

    Completed project's configuration with babel

--- WhereAmI/webpack.config.js
+++ WhereAmI/webpack.config.js
@@ -14,4 +14,18 @@ module.exports = {
             static: path.resolve(__dirname),
       },
       plugins: [new CleanPlugin.CleanWebpackPlugin()],
+      module: {
+            rules: [
+                  {
+                        test: /\.(?:js|mjs|cjs)$/,
+                        exclude: /node_modules/,
+                        use: {
+                              loader: 'babel-loader',
+                              options: {
+                                    presets: [['@babel/preset-env', { targets: 'defaults' }]],
+                              },
+                        },
+                  },
+            ],
+      },
 };
--- WhereAmI/webpack.config.prod.js
+++ WhereAmI/webpack.config.prod.js
@@ -11,4 +11,18 @@ module.exports = {
       },
       devtool: 'cheap-source-map',
       plugins: [new CleanPlugin.CleanWebpackPlugin()],
+      module: {
+            rules: [
+                  {
+                        test: /\.(?:js|mjs|cjs)$/,
+                        exclude: /node_modules/,
+                        use: {
+                              loader: 'babel-loader',
+                              options: {
+                                    presets: [['@babel/preset-env', { targets: 'defaults' }]],
+                              },
+                        },
+                  },
+            ],
+      },
 };
