3829d9e1e79257958adf94081cd5b298be397060
Edoardo Oranger <edo.oranger@gmail.com>
2024-06-01 18:43:56 +0200

    Created application skeleton

---
+++ WhereAmI/src/index.js
@@ -0,0 +1,9 @@
+import './styles/app.css';
+import './app';
+import './assets/site.json';
+import './assets/android-chrome-512x512.png';
+import './assets/android-chrome-192x192.png';
+import './assets/favicon.ico';
+import './assets/favicon-16x16.png';
+import './assets/favicon-32x32.png';
+import './assets/apple-touch-icon.png';
