184dbcb1c94125d70e7fee24e21b865c07b5bf7d
Edoardo Oranger <edo.oranger@gmail.com>
2024-06-01 13:18:48 +0200

    Completed setup of Eslint

--- WhereAmI/eslint.config.mjs
+++ WhereAmI/eslint.config.mjs
@@ -1,4 +1,30 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-
-export default [{ languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended];
+export default [
+      {
+            rules: {
+                  'constructor-super' : ['error'],
+                  'for-direction': ['error'],
+                  'getter-return' : ['error'],
+                  'no-cond-assign' : ['error'],
+                  'no-constructor-return' : ['error'],
+                  'no-dupe-args' : ['error'],
+                  'no-dupe-class-members': ['error'],
+                  'no-dupe-keys' : ['error'],
+                  'no-duplicate-case' : ['error'],
+                  'no-ex-assign' : ['error'],
+                  'no-inner-declarations' : ['error'],
+                  'no-new-native-nonconstructor' : ['error'],
+                  'no-self-assign' : ['error'],
+                  'no-unused-vars' : ['error'],
+                  'block-scoped-var' : ['error'],
+                  'camelcase' : ['error'],
+                  'eqeqeq' : ['error'],
+                  'func-name-matching' : ['error'],
+                  'func-names' : ['error'],
+                  'func-style' : ['error', 'declaration'],
+                  'no-empty' : ['error'],
+                  'no-undefined' : ['error'],
+                  'no-var' : ['error'],
+                  'prefer-template' : ['warning']
+            },
+      },
+];
