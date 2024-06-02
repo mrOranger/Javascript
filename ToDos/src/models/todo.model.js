9eee0087065379962a9fcff9895772e738372972
Edoardo Oranger <edo.oranger@gmail.com>
2024-06-02 11:38:41 +0200

    Created model for Todo resource

---
+++ ToDos/src/models/todo.model.js
@@ -0,0 +1,57 @@
+export class Todo {
+      constructor(id, title, description, done, createdAt, updatedAt) {
+            this._id = id;
+            this._title = title;
+            this._description = description;
+            this._done = done;
+            this._createdAt = createdAt;
+            this._updatedAt = updatedAt;
+      }
+
+      get id() {
+            return this._id;
+      }
+
+      get title() {
+            return this._title;
+      }
+
+      get description() {
+            return this._description;
+      }
+
+      get isDone() {
+            return this._done;
+      }
+
+      get createdAt() {
+            return this._createdAt;
+      }
+
+      get updatedAt() {
+            return this._updatedAt;
+      }
+
+      set title(value) {
+            this._title = value;
+      }
+
+      set description(value) {
+            this._description = value;
+      }
+
+      set isDone(value) {
+            this._done = value;
+      }
+
+      toString() {
+            return `{
+                  id : ${this.id},
+                  title : ${this.title},
+                  description : ${this.description},
+                  isDone : ${this.isDone},
+                  createdAt : ${this.createdAt},
+                  updatedAt : ${this.updatedAt}
+            }`;
+      }
+}
