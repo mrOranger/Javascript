adae8a39d1ee5f7c70357bb5cb44eecb9ddb2267
Edoardo Oranger <edo.oranger@gmail.com>
2024-06-09 22:01:43 +0200

    Completed path action

--- ToDos/src/components/actions/patchStateAction.component.js
+++ ToDos/src/components/actions/patchStateAction.component.js
@@ -1,3 +1,8 @@
+import { PatchModalComponent } from '../modals/patch-modal.component';
+import { TodoService } from '../../services/todo.service';
+import { Spinner } from '../spinner/spinner.component';
+import { Alert } from '../alerts/alert.component';
+
 export class PatchStateAction {
       constructor(htmlActioButton) {
             this.htmlActioButton = htmlActioButton;
@@ -5,8 +10,32 @@ export class PatchStateAction {
       }
 
       #setClickListener() {
-            this.htmlActioButton.addEventListener('click', () => {
-                  console.log('Patch State Action');
+            this.htmlActioButton.addEventListener('click', this.#onPatchTodo);
+      }
+
+      #onPatchTodo() {
+            const updateModal = new PatchModalComponent('Update a ToDo status', () => {
+                  Spinner.render('Loading');
+                  updateModal.remove();
+                  TodoService.updateStatus(updateModal.id)
+                        .then((response) => {
+                              Spinner.remove();
+                              const { statusCode, success } = response;
+                              if (success && statusCode === 200) {
+                                    Alert.render('ToDo updated successfully!', 'success');
+                              } else {
+                                    Alert.render(
+                                          'There is an error in unpdating process, check if the data are correct!',
+                                          'error',
+                                    );
+                              }
+                        })
+                        .catch((error) => {
+                              Alert.render('There is a network problem, please try later', 'error');
+                              Spinner.remove();
+                              console.error(error);
+                        });
             });
+            updateModal.render();
       }
 }
