974fc1c6a8348eb050e5b854663d265b78bf0968
Edoardo Oranger <edo.oranger@gmail.com>
2024-06-09 22:05:19 +0200

    Completed delete action

--- ToDos/src/components/actions/deleteAction.component.js
+++ ToDos/src/components/actions/deleteAction.component.js
@@ -1,3 +1,8 @@
+import { DeleteModalComponent } from '../modals/delete-modal.component';
+import { TodoService } from '../../services/todo.service';
+import { Spinner } from '../spinner/spinner.component';
+import { Alert } from '../alerts/alert.component';
+
 export class DeleteAction {
       constructor(htmlActioButton) {
             this.htmlActioButton = htmlActioButton;
@@ -5,8 +10,32 @@ export class DeleteAction {
       }
 
       #setClickListener() {
-            this.htmlActioButton.addEventListener('click', () => {
-                  console.log('Delete Action');
+            this.htmlActioButton.addEventListener('click', this.#onDeleteTodo);
+      }
+
+      #onDeleteTodo() {
+            const updateModal = new DeleteModalComponent('Delete a ToDo', () => {
+                  Spinner.render('Loading');
+                  updateModal.remove();
+                  TodoService.delete(updateModal.id)
+                        .then((response) => {
+                              Spinner.remove();
+                              const { statusCode, success } = response;
+                              if (success && statusCode === 200) {
+                                    Alert.render('ToDo deleted successfully!', 'success');
+                              } else {
+                                    Alert.render(
+                                          'There is an error in unpdating process, check if the data are correct!',
+                                          'error',
+                                    );
+                              }
+                        })
+                        .catch((error) => {
+                              Alert.render('There is a network problem, please try later', 'error');
+                              Spinner.remove();
+                              console.error(error);
+                        });
             });
+            updateModal.render();
       }
 }
---
+++ ToDos/src/components/modals/delete-modal.component.js
@@ -0,0 +1,97 @@
+export class DeleteModalComponent {
+      #body = document.querySelector('body');
+
+      constructor(title, callback) {
+            this._title = title;
+            this._callback = callback;
+      }
+
+      get title() {
+            return this._title;
+      }
+
+      set title(title) {
+            this._title = title;
+      }
+
+      get id() {
+            return this.modalIdInput.value;
+      }
+
+      render() {
+            this.modalDiv = document.createElement('div');
+            const modalDialogDiv = document.createElement('div');
+            const modalContentDiv = document.createElement('div');
+            const modalHeaderDiv = document.createElement('div');
+            const modalBodyDiv = document.createElement('div');
+            const modalInputDiv = document.createElement('div');
+            this.modalIdInput = document.createElement('input');
+            const modalFooterDiv = document.createElement('div');
+            const modalTitleH5 = document.createElement('h5');
+            const modalCloseButton = document.createElement('button');
+            const modalUpdateButton = document.createElement('button');
+
+            modalCloseButton.type = 'button';
+            modalCloseButton.classList.add('btn', 'btn-secondary');
+            modalCloseButton.dataset.dataBsDismiss = 'modal';
+            modalCloseButton.innerHTML = 'Close';
+            modalCloseButton.addEventListener('click', this.remove.bind(this));
+
+            modalUpdateButton.type = 'button';
+            modalUpdateButton.classList.add('btn', 'btn-primary');
+            modalUpdateButton.dataset.dataBsDismiss = 'modal';
+            modalUpdateButton.innerHTML = 'Update';
+            modalUpdateButton.addEventListener('click', this._callback);
+
+            modalFooterDiv.classList.add(
+                  'modal-footer',
+                  'flex-column',
+                  'align-items-stretch',
+                  'w-100',
+                  'gap-2',
+                  'pb-3',
+                  'border-top-3',
+            );
+            modalFooterDiv.appendChild(modalUpdateButton);
+            modalFooterDiv.appendChild(modalCloseButton);
+
+            this.modalIdInput.type = 'text';
+            this.modalIdInput.placeholder = 'ToDo id';
+            this.modalIdInput.ariaLabel = 'Id of the todo';
+            this.modalIdInput.classList.add('form-control');
+
+            modalInputDiv.classList.add('input-group', 'mb-3');
+            modalInputDiv.appendChild(this.modalIdInput);
+
+            modalBodyDiv.classList.add('modal-body', 'py-0');
+            modalBodyDiv.appendChild(modalInputDiv);
+
+            modalTitleH5.classList.add('modal-title', 'fs-5');
+            modalTitleH5.innerHTML = this.title;
+
+            modalHeaderDiv.classList.add('modal-header', 'border-bottom-0');
+            modalHeaderDiv.appendChild(modalTitleH5);
+
+            modalContentDiv.classList.add('modal-content', 'rounded-4', 'shadow');
+            modalContentDiv.appendChild(modalHeaderDiv);
+            modalContentDiv.appendChild(modalBodyDiv);
+            modalContentDiv.appendChild(modalFooterDiv);
+
+            modalDialogDiv.classList.add('modal-dialog');
+            modalDialogDiv.role = 'document';
+            modalDialogDiv.appendChild(modalContentDiv);
+
+            this.modalDiv.classList.add('modal', 'modal-sheet', 'position-absolute', 'd-block', 'p-4', 'py-md-5');
+            this.modalDiv.role = 'dialog';
+            this.modalDiv.tabIndex = -1;
+            this.modalDiv.appendChild(modalDialogDiv);
+
+            this.#body.appendChild(this.modalDiv);
+            this.#body.classList.add('hide');
+      }
+
+      remove() {
+            this.#body.classList.remove('hide');
+            this.modalDiv.remove();
+      }
+}
