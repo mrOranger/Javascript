5a5b76285c9fc70a9052e7c86b8981de9ce0ef34
Edoardo Oranger <edo.oranger@gmail.com>
2024-06-11 21:36:30 +0200

    Updated project's structure with Service Provider and Controllers.

---
+++ ToDos/backend/api/v1/middlewares/cors.middleware.js
@@ -0,0 +1,17 @@
+import { Request, Response, Next } from 'express';
+
+export class CorsMiddleware {
+      /**
+       * Apply the following middleware to the normal HTTP request workflow.
+       *
+       * @param {Request} request - Incoming HTTP request.
+       * @param {Response} response - Outgoing HTTP response.
+       * @param {() => void} next - Callback function to be called in passing to the next middleware.
+       */
+      apply(request, response, next) {
+            response.setHeader('Access-Controll-Allow-Origin', '*');
+            response.setHeader('Access-Controll-Allow-Methods', 'POST, GET, OPTIONS');
+            response.setHeader('Access-Controll-Allow-Headers', 'Content-Type');
+            next();
+      }
+}
--- ToDos/backend/index.js
+++ ToDos/backend/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { router as todoRoutes } from './routes/todo.route';
+import todoServiceProvider from './api/v1/service.provider';
 
 const application = express();
 
 application.use(bodyParser.json());
-application.use(todoRoutes);
+application.use(todoServiceProvider.routes);
+application.use(todoServiceProvider.middlewares.cors.apply);
 
 application.listen(3000, 'todo.io');
