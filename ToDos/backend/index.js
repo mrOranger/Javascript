1952e49703bb0b85ad80b965ad46eeb0dc98fde9
Edoardo Oranger <edo.oranger@gmail.com>
2024-06-13 22:00:54 +0200

    Removed custom middleware for CORS

--- ToDos/backend/index.js
+++ ToDos/backend/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { router } from './api/v1/routes/todo.route.js';
-import { CorsMiddleware } from './api/v1/middlewares/cors.middleware.js';
 
 const application = express();
 
 application.use(bodyParser.json());
-application.use(CorsMiddleware.apply);
+application.use(cors());
 application.use(router);
 
 application.listen(8000);
